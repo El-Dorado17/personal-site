8a93b5f85cafb50eb106f1e68ef8688ede067382
Ely Dorado <2018dorado@gmail.com>
2023-05-12 16:50:33 -0500

    algorithmic thinking outlined! Ready to start moday

--- src/components/self/About.js
+++ src/components/self/About.js
@@ -6,19 +6,61 @@ I plan for the about me section to contain:
 -a small snippet of my background
 -Interactive guess inputs with alert-box responses (Prefer drop down boxes. Try next time!)
 */
-//! import Photo_Template from './Photo_Template.jpg'        <img src={Photo_Template} className="pfp" alt="template for profile pic" />
+// import Photo_Template from './Photo_Template.jpg'        <img src={Photo_Template} className="pfp" alt="template for profile pic" />
 
-
+/*
 //!This project isn't the most effective for learning React given that no state will be changing... 
-//! Next: Come up with another app that will change state
+//! Next: Come up with another app that will change state 5/8
+*5/12* *ACTUALLY: What we're gonna do (instead of starting ANOTHER project) is make this application REACT-ive!! 
+* (If anyone ever reads this please bring it up)
+My new plan is to make this reactive by having data and then we can use fetch requests to bring it back in!
+This also means that this application will have an API to go along with it like my front-end capstone
 
+TODO: WIREFRAME IS DONE AND ITS REALLY GOOD! I KNOW I CAN MAKE THIS WORK!!
+
+Upon rendering, user can select facts about me and the click to see how many they got right! 
+TODO: API will have the data to render, and the fetch request in my UseEffect will bring it in! 
+
+
+useEffect(
+    () => {
+        fetch("http://localhost:8088/categories")
+            .then((response) => response.json())
+            .then((categoryArray) => {      //? <= Is the categoryArray the JSON response?
+                setCategories(categoryArray);
+            });
+    },
+    [] // When this array is empty, you are observing initial component state
+);
+
+
+The useEffect hook takes two arguments: a callback function and an array of dependencies. 
+    In this case, the callback function is an arrow function 
+    that fetches data from 
+    the "http://localhost:8088/categories" 
+    API endpoint using the fetch() method.  //?Understood
+
+Once the API data is fetched, the response object is converted to JSON format using the .json() method, 
+    and then the resulting categoryArray is passed as an argument to the setCategories function. setCategories 
+    is a state updater function that is used to set the state of the component with the fetched data. 
+
+The second argument of useEffect is an empty array, which means that the effect will 
+    only run once when the component is mounted. When the array of dependencies is empty, 
+    the useEffect hook observes only the initial component state. Since this effect doesn't 
+    depend on any props or state values, it doesn't need to be re-run when any of them change. //? Understood
+
+
+
+
+
+
+
+
+*/
 
 
 export const AboutMe = () => {
 
-    const colorCheck = () =>{
-        alert("test!")
-    }
 
 
 
