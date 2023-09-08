c5bf9aa66aff052a280ede520ba10d5dd16a6f71
Ely Dorado <2018dorado@gmail.com>
2023-09-08 11:17:41 -0500

    tailwind files

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -5,8 +5,8 @@ This module will host all my components so I can arrange them how I'd like
 This module gets imported to App.js as the final step to make the page!
 */
 import { About } from "../about/About.js"
-import { Projects } from "../projects/Projects.js"
-import { Quiz } from "../quiz/Quiz.js"
+//import { Projects } from "../projects/Projects.js"
+//import { Quiz } from "../quiz/Quiz.js"
 import { Route, Routes } from "react-router-dom"
 
 export const ApplicationViews = () => {
