896e031cf98068a8bc745be960acc21b517d83e8
Ely Dorado <2018dorado@gmail.com>
2023-10-06 14:10:11 -0500

    making good progress

--- src/components/photos/Photos.js
+++ src/components/photos/Photos.js
@@ -3,7 +3,10 @@ import React from "react";
 
 export const Photos = () =>{
 
-    return(
+    return(<>
         <p>Phishing Photos...</p>
+        <br/>
+        <p>I'll add some stock images and say something about them. Also screenshots of my projects and code</p>
+        </>
     )
 }
