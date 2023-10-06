d0fb280e47573da707455bdccf8d0f37803e287c
Ely Dorado <2018dorado@gmail.com>
2023-10-06 14:06:19 -0500

    brainstorming

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,14 @@
 export const Home = () =>{
     
 
-    return(
+    return(<>
         <h1> This is the Home Page </h1>
+
+        <br/>
+        <p>Here I'll have a background image with my name front and center</p>
+        <p>  ----Ely Dorado----   </p>
+        <p>  --Software Developer--   </p>
+        <p>maybe as the user scrolls it'll reveal an image of me?</p>
+        </>
     )
 }
