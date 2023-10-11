35e82026e6f5ea11af6a7aed2be80caaa70b1d69
Ely Dorado <2018dorado@gmail.com>
2023-10-11 15:19:07 -0500

    Landing Page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,14 +3,16 @@ export const Home = () =>{
 
     return(<>
     <section className= "min-h-screen bg-[url('https://www.techrepublic.com/wp-content/uploads/2012/02/ScreenShot9.png')] bg-cover bg-center bg-fixed ">
-        <article className=" h-80 bg-red-200 p-10">
-            <br/>
-            <p className="text-6xl flex items-center justify-center p-6">Ely Dorado</p>
-            <p className="text-3xl text-center">Software Developer</p>
+        <article className=" bg-green-200 bg-opacity-80 p-10 flex flex-col items-center justify-center bg-cover bg-center " style={{ backgroundImage: 'url("h.JPG")'}}>
+            
+            <p className="text-6xl mb-6">Ely Dorado </p>
+            
+            <p className="text-4xl ">Software Developer</p>
 
         </article>
+        <br/>
         <div>
-
+            <p>next</p>
         </div>
     </section>
         </>
