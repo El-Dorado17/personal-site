e7b73d4ae465792c15a9ac36b4185e28caef4473
Ely Dorado <2018dorado@gmail.com>
2023-10-11 16:40:08 -0500

    Landing Page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,8 +2,8 @@ export const Home = () =>{
     
 
     return(<>
-    <section className= " min-h-screen bg-[url('https://www.techrepublic.com/wp-content/uploads/2012/02/ScreenShot9.png')] bg-cover bg-center bg-fixed ">
-        <article title="this is my name" className=" bg-green-200 h-80 p-80  flex flex-col items-center justify-center bg-cover bg-center ">
+    <section className= " min-h-screen bg-[url('https://www.techrepublic.com/wp-content/uploads/2012/02/ScreenShot9.png')] bg-cover bg-center bg-fixed p-10">
+        <article title="this is my name" className=" bg-green-200 h-80 p-60  flex flex-col items-center justify-center bg-cover bg-center ">
             
             <p className=" text-6xl mb-6 ">Ely Dorado </p>
             
--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const NavBar = () =>{
 
     return(
-        <ul className="bg-green-200 flex justify-center space-x-8">
+        <ul className="bg-purple-200 flex justify-center space-x-8 p-10">
             <li> <Link to="/"> Home </Link> </li>
             <li> <Link to="/About"> About Me </Link> </li>
             <li> <Link to="/Projects"> Projects </Link> </li>
