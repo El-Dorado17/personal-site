e19ee3c83bf05dcf069ae6471ed095de7264208a
Ely Dorado <2018dorado@gmail.com>
2023-10-11 15:26:23 -0500

    Landing Page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,18 +2,28 @@ export const Home = () =>{
     
 
     return(<>
-    <section className= "min-h-screen bg-[url('https://www.techrepublic.com/wp-content/uploads/2012/02/ScreenShot9.png')] bg-cover bg-center bg-fixed ">
-        <article className=" bg-green-200 bg-opacity-80 p-10 flex flex-col items-center justify-center bg-cover bg-center " style={{ backgroundImage: 'url("h.JPG")'}}>
+    <section className= " min-h-screen bg-[url('https://www.techrepublic.com/wp-content/uploads/2012/02/ScreenShot9.png')] bg-cover bg-center bg-fixed ">
+        <article title="this is my name" className=" bg-green-200 h-80 p-10 flex flex-col items-center justify-center bg-cover bg-center ">
             
-            <p className="text-6xl mb-6">Ely Dorado </p>
+            <p className=" text-6xl mb-6 ">Ely Dorado </p>
             
-            <p className="text-4xl ">Software Developer</p>
+            <p className=" text-4xl ">Software Developer</p>
 
         </article>
         <br/>
-        <div>
+        <article className="bg-blue-200 h-80 p-10" >
             <p>next</p>
-        </div>
+        </article>
+
+        <br/>
+        <article className="bg-blue-200 h-80 p-10" >
+            <p>next</p>
+        </article>
+
+        <br/>
+        <article className="bg-blue-200 h-80 p-10" >
+            <p>next</p>
+        </article>
     </section>
         </>
     )
