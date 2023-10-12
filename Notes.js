1808002da54dd30270b26d3eb2a4655254fd3042
Ely Dorado <2018dorado@gmail.com>
2023-10-12 14:25:56 -0500

    project section structured

--- Notes.js
+++ Notes.js
@@ -86,18 +86,6 @@ The second argument of useEffect is an empty array, which means that the effect 
     the useEffect hook observes only the initial component state. Since this effect doesn't 
     depend on any props or state values, it doesn't need to be re-run when any of them change. //? Understood
 
-
-
-
-
-
-
-
-
-
-
-
-
 Tailwind, CSS, HTML, SQL?, 
 
 
@@ -144,6 +132,74 @@ GitHub
 
 
 
+? ____COPY & PASTE BULLETS____
+» double arrow
+• solid circle
+‣ sharp triangle
+
+
+
+
+
+!this is some code that I can come back too if my new layout doesn't pan out
+
+Desc having links to video and code
+
+Desc    Image
+Image   Desc
+Desc    Image
+
+
+        <article className="bg-blue-200 h-screen p-10 " >
+            <p className=" flex justify-center text-3xl pb-2">PROJECTS</p>
+
+            <div className="border-solid border-4 border-pink-500 p-2">
+                <div>
+                <section className="flex justify-between ">
+                    <div className="w-1/3 ">
+                        <h1>Quest Keeper</h1>
+                        <h3>Tech Used: JavaScript & React, JSON Server, HTML, Tailwind-CSS</h3>
+                        <p>QK is a full CRUD application that allows users to track their video game progress.</p>
+                        
+                    </div>
+                    <div className="border-solid border-2 border-gray-900">
+                        <img src=""/>
+                        *pic goes here*
+                    </div>
+                </section>
+                <p className="flex justify-end pb-6"> *links to code, video, and deployed*</p>
+                </div>
+
+                <div>
+                <section className="flex justify-between ">
+                    <div className="border-solid border-2 border-gray-900">
+                        <img src=""/>
+                        *pic goes here*
+                    </div>
+                    
+                    <div className="w-1/3 ">
+                        <h1>Study Other Languages (S.O.L.)</h1>
+                        <h3>Tech Used: JavaScript, React, JSON Server, HTML, CSS</h3>
+                        <p>SOL was my first project I made to show my knowledge of front-end tech. JSON Server handles  pb-6requests. Create ands Delete only, video, and deployed </p>
+                    </div>
+                </section>
+
+                </div>
+                
+                <section className="flex justify-between ">
+                <div className="w-1/3 ">
+                        <h1>Vacation Station</h1>
+                        <h3>Tech Used: JavaScript & React, Python & Django, SQLite, HTML & CSS</h3>
+                        <p>Vacation Station is my first full-stack application with full CRUD functionality</p>
+                        
+                    </div>
+                    <div className="border-solid border-2 border-gray-900">
+                        <img src=""/>
+                        *pic goes here*
+                    </div>
+                </section>
+            </div>
+        </article>
 
 
 
@@ -152,12 +208,8 @@ GitHub
 
 
 
-»
 
 
-
-
-•
 
 
 
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -55,25 +55,55 @@ const navigate = useNavigate()
 
         <br/>
 
-        <article className="bg-blue-200 h-screen p-10 " >
-            <p className=" flex justify-center text-3xl">PROJECTS</p>
-                <div className="">
-                    <section className="border-solid-2">
-                        <div>
-                            project 1 SOL
-                        </div>
-                    </section>
-                    <section>
-                        <div>
-                            project 2 QK
-                        </div>
-                    </section>
-                    <section>
-                        <div>
-                            project 3 VS
-                        </div>
-                    </section>
+        <article className="bg-blue-200 h-screen p-4" >
+            <p className=" flex justify-center text-3xl pb-2">PROJECTS</p>
+
+            <div className="p-2">
+                <div>
+                <section className="flex justify-between ">
+                    <div className="w-1/3 ">
+                        <h1>Quest Keeper</h1>
+                        <h3>Tech Used: JavaScript & React, JSON Server, HTML, Tailwind-CSS</h3>
+                        <p>QK is a full CRUD application that allows users to track their video game progress.</p>
+                        
+                    </div>
+                    <div className="border-solid border-2 border-gray-900">
+                        <img src=""/>
+                        *pic goes here*
+                    </div>
+                </section>
+                <p className="flex justify-end pb-6"> *links to code, video, and deployed*</p>
                 </div>
+
+                <div>
+                <section className="flex justify-between ">
+                <div className="w-1/3 ">
+                        <h1>Study Other Languages (S.O.L.)</h1>
+                        <h3>Tech Used: JavaScript, React, JSON Server, HTML, CSS</h3>
+                        <p>SOL was my first project I made to show my knowledge of front-end tech. JSON Server handles  pb-6requests. Create ands Delete only, video, and deployed </p>
+                    </div>
+                    <div className="border-solid border-2 border-gray-900">
+                        <img src=""/>
+                        *pic goes here*
+                    </div>
+                </section>
+                <p className="flex justify-end pb-6"> *links to code, video, and deployed*</p>
+                </div>
+
+                <section className="flex justify-between ">
+                <div className="w-1/3 ">
+                        <h1>Vacation Station</h1>
+                        <h3>Tech Used: JavaScript & React, Python & Django, SQLite, HTML & CSS</h3>
+                        <p>Vacation Station is my first full-stack application with full CRUD functionality</p>
+                        
+                    </div>
+                    <div className="border-solid border-2 border-gray-900">
+                        <img src=""/>
+                        *pic goes here*
+                    </div>
+                </section>
+                <p className="flex justify-end pb-6"> *links to code, video, and deployed*</p>
+            </div>
         </article>
 
         <br/>
