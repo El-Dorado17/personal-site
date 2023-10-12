2a35e6a751440edab4c28c3f392de23d93e8a327
Ely Dorado <2018dorado@gmail.com>
2023-10-12 10:10:58 -0500

    easter egg planted

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,41 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
 export const Home = () =>{
     
+const navigate = useNavigate()
+
+    var pattern = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight'];
+    var current = 0;
+    
+    var keyHandler = function (event) {
+    
+        // If the key isn't in the pattern, or isn't the current key in the pattern, reset
+        if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
+            current = 0;
+            return;
+        }
+    
+        // Update how much of the pattern is complete
+        current++;
+    
+        // If complete, alert and reset
+        if (pattern.length === current) {
+            current = 0;
+            navigate("/Quiz")
+        }
+    
+    };
+
+//      // Add the event listener to document
+//     document.addEventListener('keydown', keyHandler, false);
+
+//      // Remove the event listener when the component unmounts
+//     return () => {
+//         document.removeEventListener('keydown', keyHandler, false);
+//     };
+// }, [navigate]);
+
 
     return(<>
     <section className= " min-h-screen bg-[url('https://www.techrepublic.com/wp-content/uploads/2012/02/ScreenShot9.png')] bg-cover bg-center bg-fixed p-10">
@@ -48,6 +84,7 @@ export const Home = () =>{
         <article className="bg-blue-200 h-80 p-10" >
             <p>Contact</p>
         </article>
+        {document.addEventListener('keydown', keyHandler, false)}
     </section>
         </>
     )
