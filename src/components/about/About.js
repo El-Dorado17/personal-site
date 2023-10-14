bcc1af8b7e524667a486a23772298bdccb018265
Ely Dorado <2018dorado@gmail.com>
2023-10-14 17:57:32 -0500

    about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,6 +1,3 @@
-//Module will have a picture of me, and biography
-
-import pfpImage from "./pfp.jpg"
 import "../about/About.css"
 import Ely from '/home/dorado/workspace/personal-site/src/images/personal/Resume Picture (cropped).jpg'
 
@@ -13,9 +10,23 @@ export const About = () => {
             <article>
 
                 <br/> <br/> <br/>
-                <div className=" ">
-                    <img src={Ely} alt="self portrait" className="h-80 pl-2 picture" />
-                    <p className="pl-4 text-yellow-500">Ely Dorado</p>
+                <div className="flex justify-between">
+                    <section>
+                        <img src={Ely} alt="self portrait" className="h-80 pl-2 picture" />
+                        <p className="pl-4 text-yellow-500">Ely Dorado</p>
+                    </section>
+                    <section className="w-3/6 p-2 text-yellow-500">
+                        <h1>Bio</h1>
+                        <p>Hello, my name is Ely and I love software! </p>
+                            <ul>
+                                <li>Born in Texas</li>
+                                <li>Experience in construction, manufacturing, & food service </li>
+                                <li>Love playing games in VR</li>
+                                <li>Younger than the internet </li>
+                                <li>Drummer</li>
+                                <li>Fantasy movies/games/books are my favorite </li>
+                            </ul>
+                    </section>
                 </div>
 
                 <section>
