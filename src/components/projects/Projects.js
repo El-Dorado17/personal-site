7deb307e4947ed62e2a84c37a24a239ca0cd34ab
Ely Dorado <2018dorado@gmail.com>
2023-10-15 17:07:42 -0500

    tab icon changed

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -7,37 +7,29 @@ export function Projects() {
     return(<>
 <br/> <br/> <br/> <br/> <br/> <br/>
 
-<div id="default-carousel" className="relative w-full" data-carousel="slide">
-    {/* <!-- Carousel wrapper --> */}
-    <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-         {/* <!-- Item 1 --> */}
+//? Images not displaying; reference:
+//! https://flowbite.com/docs/components/carousel/
+
+<div id="default-carousel" className="relative w-full border border-gray-900" data-carousel="slide">
+    
+    <div className="relative h-56 overflow-hidden rounded-lg md:h-96 ">
         <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="https://i.pinimg.com/originals/16/6a/fa/166afa3d37dbbbeb179b5a3a3d658e10.png" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+            <img src="https://i.pinimg.com/originals/16/6a/fa/166afa3d37dbbbeb179b5a3a3d658e10.png" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="sponge pic"/>
         </div>
-        {/* <!-- Item 2 --> */}
+
         <div className="hidden duration-700 ease-in-out" data-carousel-item>
+            <p>slide two</p>
             <img src={pfp} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
         </div>
-        {/* <!-- Item 3 --> */}
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src={pfp} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-        {/* <!-- Item 4 --> */}
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src={pfp} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-        {/* <!-- Item 5 --> */}
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src={pfp} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
+
     </div>
     {/* <!-- Slider indicators --> */}
     <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
+        <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0">1</button>
+        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1">2</button>
+        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2">3</button>
+        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3">4</button>
+        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4">5</button>
     </div>
     {/* <!-- Slider controls --> */}
     <button type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
