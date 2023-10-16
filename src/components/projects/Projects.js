1bbccb47a0aebee8d7dcae5b098fff7f7a11d04f
Ely Dorado <2018dorado@gmail.com>
2023-10-16 15:27:30 -0500

    carousel feature

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,7 +2,7 @@
 import pfp from '/home/dorado/workspace/personal-site/src/components/about/pfp.jpg'
 import example from '/home/dorado/workspace/personal-site/src/images/images.jpg'
 import React from "react"
-
+import { useState } from 'react'
 
 //? Images not displaying; reference:
 //! https://flowbite.com/docs/components/carousel/
@@ -17,18 +17,33 @@ const images = [
 
 
 export function Projects() {
-    
+
+    const [currentIndex, setCurrentIndex] = useState(0)
+
+    const prevSlide = () =>{
+        const isFirstSlide = currentIndex === 0;
+        const newIndex = isFirstSlide ? images.length -1 : currentIndex -1
+        setCurrentIndex(newIndex)
+    }
+
+    const nextSlide = () =>{
+        const isLastSlide = currentIndex === images.length -1
+        const newIndex = isLastSlide ? 0 : currentIndex + 1
+        setCurrentIndex(newIndex)
+    }
+
+
 return(<>
     <br/> <br/> <br/> <br/> <br/>
 
-    <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative'> 
+    <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'> 
         
-        <div style={{backgroundImage:`url(${images[0].url})`}} className='w-full h-full rounded-2xl bg-center bg-cover duration-500'>
+        <div style={{backgroundImage:`url(${images[currentIndex].url})`}} className='w-full h-full rounded-2xl bg-center bg-cover duration-500'>
 
         </div>
 
 {/* Left Button */}
-        <button type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+        <button onClick={prevSlide} type="button" className="hidden group-hover:block absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
             <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                 <svg className="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4"/>
@@ -38,7 +53,7 @@ return(<>
         </button>
 
 {/* Right Button */}
-        <button type="button" className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next> 
+        <button onClick={nextSlide} type="button" className="hidden group-hover:block absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next> 
             <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                 <svg className="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
@@ -50,35 +65,3 @@ return(<>
         </>
     )
 }
-
-
-
-
-
-
-
-
-<div id="default-carousel" className="relative w-full border border-gray-900" data-carousel="slide">
-    
-    <div className="relative h-56 overflow-hidden rounded-lg md:h-96 ">
-        <div className="hidden duration-700 ease-in-out" data-carousel-item="active">
-            <img src={example} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="sponge pic"/>
-        </div>
-
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <p>slide two</p>
-            <img src={pfp} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-
-    </div>
-    {/* <!-- Slider indicators --> */}
-    <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-        {/* <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button> */}
-    </div>
-    {/* <!-- Slider controls --> */}
-
-</div>
