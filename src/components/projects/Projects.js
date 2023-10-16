d8b3de0710f4d66c32ef82748f7a78020418f1c9
Ely Dorado <2018dorado@gmail.com>
2023-10-16 16:01:53 -0500

    carousels and proj details in respective modules

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,28 +1,5 @@
 import React from "react"
-import { useState } from 'react'
 
-import QKAllUsers from '/home/dorado/workspace/personal-site/src/images/qk/all-users-qk.png'
-import QKForm from '/home/dorado/workspace/personal-site/src/images/qk/form-qk.png'
-import QKMyPage from '/home/dorado/workspace/personal-site/src/images/qk/my-page.png'
-import QKWelcome from '/home/dorado/workspace/personal-site/src/images/qk/qk welcome.png'
-import QKCode from '/home/dorado/workspace/personal-site/src/images/qk/qk-code.png'
-import QKRegister from '/home/dorado/workspace/personal-site/src/images/qk/register-qk.png'
-import QKSignIn from '/home/dorado/workspace/personal-site/src/images/qk/sign-in-qk.png'
-
-import SOLCreate from '/home/dorado/workspace/personal-site/src/images/sol/create sol.png'
-import SOLFiltered from '/home/dorado/workspace/personal-site/src/images/sol/filtered sol.png'
-import SOLMain from '/home/dorado/workspace/personal-site/src/images/sol/mian sol.png'
-import SOLRegister from '/home/dorado/workspace/personal-site/src/images/sol/register sol.png'
-import SOLCode from '/home/dorado/workspace/personal-site/src/images/sol/sol code.png'
-import SOLStudent from '/home/dorado/workspace/personal-site/src/images/sol/student sol.png'
-
-// import from ''
-// import from ''
-// import from ''
-// import from ''
-// import from ''
-// import from ''
-// import from ''
 
 import { QuestKeeper } from "./QuestKeeper"
 
---
+++ src/components/projects/VacationStation.js
@@ -0,0 +1,12 @@
+//Specific Modules had to be made to prevent image carousels from cycling incorectly
+
+import React from "react"
+import { useState } from 'react'
+
+// import from ''
+// import from ''
+// import from ''
+// import from ''
+// import from ''
+// import from ''
+// import from ''
