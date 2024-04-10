0526f629130d0696867fb677f7e77f68723dfe43
Ely Dorado <2018dorado@gmail.com>
2024-04-09 19:30:19 -0500

    Resume completed git push origin main

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -7,17 +7,16 @@ import res from '../../components/resume/Ely-Dorado.pdf'
 export const Resume =()=>{
 
 return(
-<section>
+<section className= " bg-cover bg-center bg-fixed p-5 md:p-10 bg-gradient-to-l from-gray-500 to-black" >
     <div>
-        <p>p1</p>
         <p>p1</p>
         <p>p1</p>
     </div>
 
-    <article>
-        <h1> Hello! Thanks for checking out my resume section!</h1>
-        <div className="pb-2 flex items-center hover:underline">
-            <iframe src={res} title="Resume PDF" width="50%" height="500px" className=""  />
+    <article className="bg-gray-700 pt-2 md:p-6 rounded-xl text-yellow-500">
+        {/* <h1> Hello! Thanks for checking out my resume section!</h1> */}
+        <div className="pb-2 flex justify-center hover:underline">
+            <iframe src={res} title="Resume PDF" width="60%" height="800px" className=""  />
         </div> 
 
             <a href={Resume} download="Ely Dorado - Resume" className="flex justify-center"> <button className="bg-gray-600 p-2 hover:bg-yellow-400 hover:text-black rounded-full"> Download Resume </button> </a>
